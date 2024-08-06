c687d8406d0caf8bcbd28e51a27bc39e7d5e83f3
Andres Ortega <aortega473@soyudemedellin.edu.co>
2024-08-06 13:12:30 -0500

    Add class Triangle

--- painter/model.py
+++ painter/model.py
@@ -11,10 +11,10 @@ class Circle:
         self.center = center
         self.radius = radius
     
-    def area(self)-> float:
+    def Area(self)-> float:
         return math.pi * self.radius ** 2
     
-    def draw(self):
+    def Draw(self):
         circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
         plt.gca().add_patch(circle)
         plt.axis("scaled")
@@ -23,3 +23,23 @@ class Circle:
     def _str_(self):
         return f"Circle with center at ({self.center.x}, {self.center.y}) and radius {self.radius}"
     
+class Triangle: 
+    def _init_(self, point_1: Point, point_2: Point, point_3: Point): 
+        self.point_1 = point_1 
+        self.point_2 = point_2 
+        self.point_3 = point_3 
+        
+    def Area(self)-> float: 
+        return abs((self.point_1.x * (self.point_2.y - self.point_3.y) + 
+                    self.point_2.x * (self.point_3.y - self.point_1.y) + 
+                    self.point_3.x * (self.point_1.y - self.point_2.y)) / 2) 
+
+    def Draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x] 
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y] 
+        plt.fill(x, y, color='b') 
+        plt.axis("scaled") 
+        plt.show() 
+        
+    def _str_(self): 
+        return f"Triangle with vertices at ({self.point_1.x}, {self.point_1.y}), ({self.point_2.x}, {self.point_2.y}), and ({self.point_3.x}, {self.point_3.y})"
